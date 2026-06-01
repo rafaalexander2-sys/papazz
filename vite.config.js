a280e3ce45970cabbc83d18c7c680042f80a6cc4
rafaalexander2-sys <rafaalexander2@gmail.com>
2026-06-01 20:16:06 -0300

    feat: add sitemap.xml and react-snap prerender for SEO indexing

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import mdx from "@mdx-js/rollup";
 import remarkGfm from "remark-gfm";
 import rehypeHighlight from "rehype-highlight";
-
 export default defineConfig({
   plugins: [
     {
